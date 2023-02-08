78d07c3595e9a1e92b9d58d71d98a8b13778be75
Brook Endale <bekelebrook24@gmail.com>
2023-02-08 20:10:49 +0300

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,10 +8,12 @@ def minOperations(n):
     '''Returns the minimum number of operations
     needed to get n number of characters in a file
     '''
+    if type(n) is not int or n < 2:
+        return 0
     m = n
     for i in range(n//2):
         if i > 0:
             k = n / i
             if k == int(k) and k + i < m:
                 m = int(k + i)
-    return m if m > 1 else 0
+    return m
