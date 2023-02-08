2a0209a6ca28dbfd93f5d1872fd53a4293542f55
Brook Endale <bekelebrook24@gmail.com>
2023-02-08 19:22:35 +0300

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-'''This module conatains a function that does the minimum operations challange'''
+'''This module conatains a function that does the minimum operations
+to get the given number of characters in a file.
+'''
 
 
 def MinOperations(n):
     '''Returns the minimum number of operations
+    needed to get n number of characters in a file
     '''
     m = n
     for i in range(n//2):
         if i > 0:
             k = n / i
             if k == int(k) and k + i < m:
-                    m = int(k + i)
+                m = int(k + i)
     return m
