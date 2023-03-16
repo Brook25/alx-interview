fb7f1c6efb406a2104e3ee806630e9014aa569d4
Brook25 <bekelebrook24@gmail.com>
2023-03-16 12:11:38 +0000

    Rotate 3-d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,12 +9,10 @@ def rotate_2d_matrix(matrix):
     2-D matrix 90 degrees
     '''
     mat_mirr = list(map(lambda x: list(map(lambda y: y, x)), matrix))
-    i = 0
-    j = len(matrix) - 1
+    i, j = 0, len(matrix) - 1
     while i < len(matrix):
         k = 0
         while k < len(matrix):
             matrix[k][j] = mat_mirr[i][k]
             k += 1
-        i += 1
-        j -= 1
+        i, j = i + 1, j - 1
