0dbf7e4cd025878013319085b1cdcfe07237e8c2
Brook25 <bekelebrook24@gmail.com>
2023-03-21 06:46:41 +0000

    edit

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,14 +5,11 @@ a 2-D matrix 90 degrees.
 
 
 def rotate_2d_matrix(matrix):
-    '''Function rotates
-    2-D matrix 90 degrees
+    '''Function rotates 2-D matrix 90 degrees
     '''
     mat_mirr = list(map(lambda x: list(map(lambda y: y, x)), matrix))
     i, j = 0, len(matrix) - 1
     while i < len(matrix):
-        k = 0
-        while k < len(matrix):
+        for k in range(len(matrix)):
             matrix[k][j] = mat_mirr[i][k]
-            k += 1
         i, j = i + 1, j - 1
