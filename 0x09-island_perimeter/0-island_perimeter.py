ec0103bafc2b22cf22619e87cd5f15e1e8d0bed5
Brook25 <bekelebrook24@gmail.com>
2023-04-02 10:08:54 +0000

    edits

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,12 +11,12 @@ def island_perimeter(grid):
         if 1 in grid[y]:
             for x in range(len(grid[0])):
                 if grid[y][x] == 1:
-                    if y == len(grid) - 1 or grid[y+1][x] != 1:
+                    if y == len(grid) - 1 or not grid[y+1][x]:
                         perim += 1
-                    if y == 0 or grid[y-1][x] != 1:
+                    if y == 0 or not grid[y-1][x]:
                         perim += 1
-                    if x == 0 or grid[y][x-1] != 1:
+                    if x == 0 or not grid[y][x-1]:
                         perim += 1
-                    if x == len(grid[0]) - 1 or grid[y][x+1] != 1:
+                    if x == len(grid[0]) - 1 or not grid[y][x+1]:
                         perim += 1
     return perim
