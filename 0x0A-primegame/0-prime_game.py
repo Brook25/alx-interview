1dfa90686d70df4d8a6df798e41f3c20c1d0e467
Brook25 <bekelebrook24@gmail.com>
2023-04-17 16:03:23 +0000

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,15 +5,16 @@ the prime game challange
 
 def isWinner(x, nums):
     '''Fucntion does the prime game challange'''
-    dct = {'M': 0, 'B': 0}
-    for i in range(x):
-        vals, x = {0, 1}, 'B'
-        for j in range(nums[i] + 1):
-            if j not in vals:
-                x = 'M' if x == 'B' else 'B'
-                vals.update(set(filter(lambda x: x % j == 0,
-                    set(range(j, nums[i] + 1)))))
-        dct[x] += 1
-    if dct['M'] == dct['B']:
-        return None
-    return 'Maria' if dct['M'] > dct['B'] else 'Ben'
+    if x and nums:
+        dct = {'M': 0, 'B': 0}
+        for i in range(x):
+            vals, x = {0, 1}, 'B'
+            for j in range(nums[i] + 1):
+                if j not in vals:
+                    x = 'M' if x == 'B' else 'B'
+                    vals.update(set(filter(lambda x: x % j == 0,
+                        set(range(j, nums[i] + 1)))))
+            dct[x] += 1
+        if dct['M'] == dct['B']:
+            return None
+        return 'Maria' if dct['M'] > dct['B'] else 'Ben'
