c2b69fa0d4f0cb5ca1e04198ca5849d3e7100b4f
Brook25 <bekelebrook24@gmail.com>
2023-04-17 17:35:04 +0000

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,18 +5,32 @@ the prime game challange
 
 def isWinner(x, nums):
     '''Fucntion does the prime game challange'''
-    if nums:
-        dct = {'M': 0, 'B': 0}
-        for i in range(x):
-            vals, x = {0, 1}, 'B'
-            if i >= len(nums):
-                break
-            for j in range(nums[i] + 1):
-                if j not in vals:
-                    x = 'M' if x == 'B' else 'B'
-                    vals.update(set(filter(lambda x: x % j == 0,
-                        set(range(j, nums[i] + 1)))))
-            dct[x] += 1
-        if dct['M'] == dct['B']:
-            return None
-        return 'Maria' if dct['M'] > dct['B'] else 'Ben'
+    dct = {'M': 0, 'B': 0}
+    if nums[0] == 0:
+        return None
+    for i in range(x):
+        vals, x = {0, 1}, 'B'
+        for j in range(nums[i] + 1):
+            if j not in vals:
+                x = 'M' if x == 'B' else 'B'
+                vals.update(set(filter(lambda x: x % j == 0,
+                    set(range(j, nums[i] + 1)))))
+        dct[x] += 1
+    if dct['M'] == dct['B']:
+        return None
+    return 'Maria' if dct['M'] > dct['B'] else 'Ben'
+
+
+#nums = [0] * 10000
+#for i in range(10000):
+#    nums[i] = i
+
+
+#nums = [0] * 100
+#for i in range(100):
+#    nums[i] = i * i
+
+#print("Winner: {}".format(isWinner(100, nums)))
+
+
+#print("Winner: {}".format(isWinner(10000, nums)))
