11bb93cdef3a4c58e88042c10e8eab96de14a399
Brook Endale <bekelebrook24@gmail.com>
2023-08-03 20:27:28 +0300

    edit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""N-queens challange using backtracking algorithm.
-   Without using much constraint propagation or heuristic selection.
+"""N-queens challange using backtracking algorithm without
+    much constraint propagation or heuristic selection.
 """
 import sys
 
@@ -8,21 +8,19 @@ import sys
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
-
 try:
     n = int(sys.argv[1])
 except ValueError:
     print("N must be a number")
     sys.exit(1)
-
 if n < 4:
     print("N must be at least 4")
     sys.exit(1)
 
 
 def main():
-    '''Main function to print out all possible solutions
-    of a given N queen puzzle
+    '''Main function to print out all possible
+    solutions of a given N queen puzzle
     '''
     for i in range(n):
         lst2 = [[0, i]]
@@ -30,7 +28,8 @@ def main():
 
 
 def chk_queens(lst2, lst_col, n):
-    '''recursively checks for possible mathces'''
+    '''recursively checks for possible mathces
+    '''
     if len(lst2) == n:
         print(lst2)
         return
@@ -44,20 +43,17 @@ def chk_queens(lst2, lst_col, n):
 
 
 def chk_mate(lst1, lst2):
-    '''checks if two spots can hhold queens
-       without attacking eachother
+    '''checks if two spots can hhold queens without attacking eachother
     '''
     for lst in lst1:
         if lst[1] == lst2[1]:
             return False
-
         if lst[0] < lst2[0] and lst[1] < lst2[1]:
             n = lst2[0] - lst[0]
             if lst2[1] == lst[1] + n:
                 return False
             else:
                 continue
-
         if lst[0] < lst2[0] and lst[1] > lst2[1]:
             n = lst2[0] - lst[0]
             if lst2[1] == lst[1] - n:
