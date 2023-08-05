7f5607d09fed22e0097266507afc94f3af844af3
Brook25 <bekelebrook24@gmail.com>
2023-08-05 20:02:09 +0000

    redo task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-"""Determine the fewest number of coins needed to meet a given
-amount total"""
+"""Making change
+"""
 
 
 def makeChange(coins, total):
-    """Return fewest number of coins needed to meet total"""
-
+    """make change function"""
     if total <= 0:
-        return (0)
+        return 0
+    coins = sorted(list(set(coins)), reverse=True)
+    ret = None
+    for i in range(len(coins)):
+        if coins[i] <= total:
+            if ret and ret <= total // coins[i]:
+                return ret
+            change = getNumCoin(total % coins[i], coins[i:], total // coins[i])
+            if not ret and change:
+                ret = change
+            if change and ret and change < ret:
+                ret = change
+    return -1
 
-    coins.sort(reverse=True)
 
-    count = 0
-    tmp = 0
-
-    for i in coins:
-        tmp = int(total / i)
-        total = total - (tmp * i)
-        count += tmp
-        if total == 0:
-            return (count)
-    if total != 0:
-        return (-1)
+def getNumCoin(div, coins, count):
+    """returns number of coins for an iteration"""
+    for coin in coins:
+        if coin > div:
+            continue
+        if not div % coin:
+            return count + div // coin
+        ret = getNumCoin(div % coin , coins, count + div // coin)
+        if ret:
+            return ret
